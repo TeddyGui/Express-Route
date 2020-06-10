05d7ccf3852be3ec8037050e41e4d5423dbe4176
TeddyGui <61049069+TeddyGui@users.noreply.github.com>
2020-06-10 09:32:06 +0200

    Add files via upload

---
+++ service.js
@@ -0,0 +1,19 @@
+const express = require('express') ;
+let hours = new Date().getHours()
+const app=express();
+
+//Site open Condition
+
+app.get('/', (req, res)=> {
+     ( (hours >=8) && (hours <=17) ) ? res.sendFile(__dirname + '/public/Home.html')
+  })
+
+
+// Static folder
+app.use(express.static(__dirname + '/public'));
+
+// Port
+
+const PORT=process.env.PORT || 5000 ;
+
+app.listen(PORT,()=> console.log( `server started on port ${PORT}` ));
